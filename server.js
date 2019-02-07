d5bc41f9c71eca7184825ab25bc5267e6bf2514f
dewale005 <olagunjuadewale5@gmail.com>
2019-02-07 15:21:42 +0100

    prod

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express=require('express');
 const app=expres();
 const path=require('path');
 
-app.us(express.static(__dirname+'/dist'));
+app.us(express.static(__dirname+'/dist/fongo'));
 
 app.listen(process.env.PORT||8080);
 
 
 //PathLocationStradegy
 app.get('/', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/fongo/index.html'));
   });
 
 console.log('Console Listening'); 
